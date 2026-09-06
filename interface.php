ee82d72884cf6cd6670fbb7af02b6f072eb161fd
Rohan Shrestha <rohan.org.com@gmail.com>
2026-09-06 18:08:53 +0545

    interface

---
+++ interface.php
@@ -0,0 +1,24 @@
+<?php
+interface MyInterface{
+    public function getName($name):
+    public function getAge($age);
+}
+
+class MyClass implements MyInterface{
+
+public function getName($name){
+
+echo"My name is $name";.'<br>';
+}
+
+public function getAge($age){
+    echo "My age is $age";
+}
+}
+$obj = new MyClass;
+$obj->getName("Rohan");
+$obj->getAge("44");
+
+
+
+?>
